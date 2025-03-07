59d009359e5537ce5b5202d812b340260eb14c60
jaxin007 <swatdimonbil@gmail.com>
2025-03-07 10:42:53 +0200

    feat(1.0.36): add `USER_COMPLIANCE_INFO` prefix and bump version to 1.0.36

--- src/common/constants/redisPrefixes.ts
+++ src/common/constants/redisPrefixes.ts
@@ -20,5 +20,6 @@ export const RedisPrefixes = {
      */
     RACING_CUSTOM_POSITIONS: 'racing_custom_positions',
     LADDERS_MAP: 'ladders_map',
+    USER_COMPLIANCE_INFO: 'user_compliance_info',
   },
 };
