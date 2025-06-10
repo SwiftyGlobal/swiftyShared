04dfc065850004b2e9963046d212a1556ab4f2c4
jaxin007 <swatdimonbil@gmail.com>
2025-06-10 17:23:38 +0300

    feat(1.0.44): add JSDoc comments for stripDecimal function

--- src/utils/stripDecimal.ts
+++ src/utils/stripDecimal.ts
@@ -1,5 +1,12 @@
 import Decimal from 'decimal.js';
 
+/**
+ * Strips a number to a maximum of 5 decimal places using the Decimal.js library.
+ *
+ * @param {number} value - The number to be stripped to 5 decimal places.
+ * @returns {Decimal} A Decimal.js object representing the number with up to 5 decimal places.
+ */
+
 export const stripDecimal = (value: number) => {
   return new Decimal(value).toDecimalPlaces(5);
 };
