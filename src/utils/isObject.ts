4205281000e610f7355f8cec25f521a1f032cc6c
jaxin007 <swatdimonbil@gmail.com>
2025-06-10 17:23:25 +0300

    feat(1.0.44): add JSDoc comments for isObject function

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,3 +1,10 @@
+/**
+ * Checks if the given value is an object.
+ *
+ * @param {any} value - The value to check.
+ * @returns {boolean} `true` if the value is an object and not an array or `null`, otherwise `false`.
+ */
+
 export const isObject = (value: any): boolean => {
   return !Array.isArray(value) && typeof value === 'object' && value !== null;
 };
