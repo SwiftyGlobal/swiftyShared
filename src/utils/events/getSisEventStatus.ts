97cf4b5d2ae42cc60a7ab75112b7043f090025a1
jaxin007 <swatdimonbil@gmail.com>
2025-06-10 17:22:28 +0300

    feat(1.0.44): add JSDoc comments for getSISEventStatus function

--- src/utils/events/getSisEventStatus.ts
+++ src/utils/events/getSisEventStatus.ts
@@ -1,6 +1,14 @@
 import { SportEventStatuses } from '../../common';
 import type { Nullable } from '../../types';
 
+/**
+ * Determines the status of a sports event based on the provided parameters.
+ *
+ * @param {Nullable<string>} offTime - The off time of the event, or null if not available.
+ * @param {boolean} isEventResulted - Indicates whether the event has been resulted.
+ * @returns {SportEventStatuses} - The status of the sports event.
+ */
+
 export const getSISEventStatus = (offTime: Nullable<string>, isEventResulted: boolean): SportEventStatuses => {
   if (isEventResulted) {
     return SportEventStatuses.FINISHED;
