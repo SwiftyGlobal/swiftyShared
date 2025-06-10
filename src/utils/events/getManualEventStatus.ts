6dec42414f5a55cda6156b536bccd9db63ec8531
jaxin007 <swatdimonbil@gmail.com>
2025-06-10 17:22:11 +0300

    feat(1.0.44): add JSDoc comments for getManualEventPhaseStatus function

--- src/utils/events/getManualEventStatus.ts
+++ src/utils/events/getManualEventStatus.ts
@@ -1,6 +1,13 @@
 import { SportEventStatuses } from '../../common';
 import type { EventPhaseStatus } from '../../types';
 
+/**
+ * Determines the manual event phase status based on the provided event phase.
+ *
+ * @param {string} eventPhase - The current phase of the event (e.g., 'InPlay', 'PreMatch', 'Pre-Match').
+ * @returns {EventPhaseStatus} An object containing the current status and phase of the event.
+ */
+
 export const getManualEventPhaseStatus = (eventPhase: string): EventPhaseStatus => {
   let current_status: SportEventStatuses = SportEventStatuses.PRE_MATCH;
   const current_phase = eventPhase;
